62048ffd681e64fa57a13e8ca0e735ff9b2a48e7
Egor <82820305+Ryagich@users.noreply.github.com>
2025-12-21 02:02:57 +0500

    Localization buyer states

--- Assets/Scripts/StateMachine/Graph/Model/State.cs
+++ Assets/Scripts/StateMachine/Graph/Model/State.cs
@@ -1,12 +1,13 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Localization;
 
 namespace StateMachine.Graph.Model
 {
     [CreateAssetMenu(fileName = "State", menuName = "configs/StateMachine/State")]
     public class State : ScriptableObject
     {
-        [field: SerializeField] public string StateName { get; private set; } = "State Name";
+        [field: SerializeField] public LocalizedString Name { get; private set; }
         
         public List<BaseBehaviour> Behaviours = new();
         public List<Transition> Transitions = new();
--- Assets/Scripts/UI/Hover/PopupLogics/Popups/BuyerPopup.cs
+++ Assets/Scripts/UI/Hover/PopupLogics/Popups/BuyerPopup.cs
@@ -45,7 +45,8 @@ namespace UI.Hover.PopupLogics.Popups
                 var holderRect = positionHolder.GetComponent<RectTransform>();
                 holderRect.anchoredPosition = new Vector2(.0f, h);
                 h -= holderRect.sizeDelta.y;
-                positionHolder.ProductName.text = $"{localizationConfig.ProductWord.GetLocalizedString()}: {buyPosition.Config.Name.GetLocalizedString()}";
+                positionHolder.ProductName.text = $"{buyPosition.Config.Name.GetLocalizedString()}";
+                // positionHolder.ProductName.text = $"{localizationConfig.ProductWord.GetLocalizedString()}: {buyPosition.Config.Name.GetLocalizedString()}";
                 positionHolder.ProductCounts.text = $"{buyPosition.Count} / {buyPosition.Need}";
                 positionHolder.Fill.fillAmount = (float)buyPosition.Count / buyPosition.Need;
 
@@ -62,7 +63,7 @@ namespace UI.Hover.PopupLogics.Popups
                     positionHolder.FillBack.color = buyerController.context.BuyerSettings.OutOfStockColor;
                 }
             }
-            popup.BuyerStatus.text = buyerController.CurrentState.StateName;
+            popup.BuyerStatus.text = buyerController.CurrentState.Name.GetLocalizedString();
             popupRect.sizeDelta = popupRect.sizeDelta.WithY(popupRect.sizeDelta.y
                                 + (buyerController.context.BuyPositions.Count 
                                  * popupHolders.BuyerProductInfo.GetComponent<RectTransform>().sizeDelta.y) 
